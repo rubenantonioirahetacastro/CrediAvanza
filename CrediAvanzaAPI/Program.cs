3635d35507f8f1adadfdaf01ba6d97e6e3f79df4
Programador16-stefanini <programador16.ext@sspcap.com>
2026-07-20 13:46:04 -0600

    FIX 2

--- CrediAvanzaAPI/Program.cs
+++ CrediAvanzaAPI/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddScoped<ISegmentoUsuraService, SegmentoUsuraService>();
 builder.Services.AddScoped<ILineaCreditoService, LineaCreditoService>();
 builder.Services.AddScoped<ISimulacionCalendarioService, SimulacionCalendarioService>();
 builder.Services.AddScoped<ErrorLogger>();
+builder.Services.AddScoped<IBlobStorageService, LocalBlobStorageService>();
 
 builder.Services.AddApplicationInsightsTelemetry(options =>
 {
